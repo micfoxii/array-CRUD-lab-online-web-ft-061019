f656f087653821a92224f5e16804671796550339
Michael Fox <michael.c.fox.ii@gmail.com>
2019-06-13 17:00:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,5 +1,5 @@
 def create_an_empty_array
-  []
+  Array.new
 end
 
 def create_an_array
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.
 end
 
 def add_element_to_start_of_array(array, element)
